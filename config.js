04a1e02b25c2c83c954c446bc390453d7d08ccca
divrawat <divrawat2001@gmail>
2024-07-02 22:28:52 +0530

    rsd0gvzdg5r

---
+++ config.js
@@ -0,0 +1,421 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://www.thedivorceesdessertcafe.in.net';
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>The Divorcees Dessert Cafe</u></a> is a romantic manga that revolves around the story of a woman who, after a tumultuous marriage and subsequent divorce, decides to start anew by opening a dessert cafe. Seeking solace and a fresh start, she pours her heart into creating delectable treats, attracting a large number of customers.`;
+
+export const MANGA_NAME = 'The Divorcees Dessert Cafe';
+export const MANGA_AUTHOR = 'Dandelionwine';
+export const MANGA_RELEASE = '2023';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Night Blue';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Romance, Drama, Comedy, Cooking';
+
+export const CHAPTER_PREFIX = 'the-divorcees-dessert-cafe-chapter';
+export const IMAGE_PREFIX = 'images/the-divorcees-dessert-cafe';
+export const NEXT_PREVIOUS_PREFIX = 'manga/the-divorcees-dessert-cafe-chapter';
+export const URL_PREFIX = 'manga/the-divorcees-dessert-cafe-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/the-divorcees-dessert-cafe-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://ukjournal.co.uk/wp-content/uploads/2024/01/The-Divorcees-Dessert-Cafe.png"
+
+// "https://waxbizz.co.uk/wp-content/uploads/2024/04/The-Divorcees-Dessert-Cafe.jpg";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] w-[72px] rounded-full mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'The Divorcees Dessert Cafe';
+export const NavbarName = "The Divorcees Dessert Cafe";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}. Hana decides to leave her past behind and move to a quaint town where she opens a charming dessert cafe. Determined to start afresh, she channels her passion for baking into creating a haven filled with delectable sweets and a warm, inviting atmosphere.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "The Divorcée's Dessert Cafe follows the journey of Hana, a woman who finds herself at a crossroads after a painful divorce. Feeling lost and heartbroken, Hana decides to leave her past behind and move to a quaint town where she opens a charming dessert cafe. Determined to start afresh, she channels her passion for baking into creating a haven filled with delectable sweets and a warm, inviting atmosphere."
+    },
+    {
+        id: 2,
+        content: "As Hana navigates the challenges of running a new business, she encounters a colorful cast of characters who become regulars at her cafe. Each customer brings their own stories and troubles, finding comfort and solace in Hana's delicious creations and her empathetic ear. Through these interactions, Hana begins to rebuild her shattered self-esteem and discovers a newfound purpose."
+    },
+    {
+        id: 3,
+        content: "One of her regulars, a kind and supportive single father named Kenji, becomes a close friend and confidant. Their bond deepens as they share their experiences and support each other through their respective struggles. Kenji’s daughter, Mei, also becomes a frequent visitor, bringing a sense of joy and innocence to the cafe."
+    },
+    {
+        id: 4,
+        content: "As Hana’s cafe gains popularity, she attracts the attention of a successful pastry chef, Yuto, who is intrigued by her talent and resilience. Yuto offers to mentor Hana, helping her refine her skills and expand her business. Their professional relationship gradually evolves into a romantic one, adding a layer of complexity to Hana’s new life."
+    },
+    {
+        id: 5,
+        content: "Throughout the story, Hana grapples with her past wounds and the fear of opening her heart again. Her journey is one of self-discovery and healing, as she learns to trust herself and others once more. The cafe becomes more than just a business; it is a place where broken hearts find refuge and new beginnings take root."
+    },
+    {
+        id: 6,
+        content: "The Divorcée's Dessert Cafe beautifully intertwines themes of resilience, personal growth, and the sweetness of second chances. It is a heartwarming tale of finding love and fulfillment in unexpected places, and the transformative power of following one’s passion."
+    }
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+export const RelatedMangaLinks = [
+    /*
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran Manga',
+        link: 'https://www.frierenmanga.in.net',
+    },
+    */
+
+];
+
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 14
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "3a",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 3
+    },
+    {
+        "chapterNumber": "4a",
+        "numImages": 2
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 43
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "6a",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 33
+    },
+    {
+        "chapterNumber": "7a",
+        "numImages": 34
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 36
+    },
+    {
+        "chapterNumber": "8a",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 35
+    },
+    {
+        "chapterNumber": "9a",
+        "numImages": 27
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 36
+    },
+    {
+        "chapterNumber": "10a",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "11a",
+        "numImages": 31
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "12a",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "13a",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 36
+    },
+    {
+        "chapterNumber": "14a",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 42
+    },
+    {
+        "chapterNumber": "15a",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 44
+    },
+    {
+        "chapterNumber": "16a",
+        "numImages": 38
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 39
+    },
+    {
+        "chapterNumber": "17a",
+        "numImages": 33
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 40
+    },
+    {
+        "chapterNumber": "18a",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 36
+    },
+    {
+        "chapterNumber": "19a",
+        "numImages": 33
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 42
+    },
+    {
+        "chapterNumber": "20a",
+        "numImages": 36
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 39
+    },
+    {
+        "chapterNumber": "21a",
+        "numImages": 37
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 39
+    },
+    {
+        "chapterNumber": "22a",
+        "numImages": 34
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 70
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 70
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 19
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 18
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "31",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "32",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "33",
+        "numImages": 3
+    },
+    {
+        "chapterNumber": "34",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "35",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "36",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "37",
+        "numImages": 4
+    },
+    {
+        "chapterNumber": "38",
+        "numImages": 6
+    },
+    {
+        "chapterNumber": "39",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "40",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "41",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "42",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "43",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "44",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "45",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "46",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "47",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "48",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "49",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "50",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "51",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "52",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "53",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "54",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "55",
+        "numImages": 5
+    },
+    {
+        "chapterNumber": "56",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "57",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "58",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "59",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "60",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "61",
+        "numImages": 10
+    }
+]
